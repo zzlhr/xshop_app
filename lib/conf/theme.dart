68c21dba83d39cb208b363303242b4110dedd54b
zzlhr <2388399752@qq.com>
2019-03-31 00:58:34 +0800

    我的页面，加入图标

--- lib/conf/theme.dart
+++ lib/conf/theme.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
 var themeColor = Colors.blue;
+
+var baseFontSize = 14.0;
+var baseFont = TextStyle(fontSize: baseFontSize);
+var h1 = TextStyle(fontSize: (baseFontSize + 6), fontWeight: FontWeight.bold);
+var h2 = TextStyle(fontSize: baseFontSize + 5, fontWeight: FontWeight.w500);
--- lib/pages/My.dart
+++ lib/pages/My.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:xshop_app/component/XShopBottomNavigation.dart';
+import 'package:xshop_app/conf/theme.dart';
 import 'package:xshop_app/pages/Login.dart';
 import 'package:xshop_app/pages/Register.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class MyPage extends StatefulWidget {
   MyPage({Key key, this.title}) : super(key: key);
@@ -20,11 +22,21 @@ class MyPageState extends State<MyPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("我的"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            tooltip: '设置',
+            onPressed: (){},
+          ),
+
+        ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           _my(),
           _order(),
+          _toolBox(),
           RaisedButton(
             child: Text("登录"),
             onPressed: () {
@@ -49,50 +61,143 @@ class MyPageState extends State<MyPage> {
     );
   }
 }
-
+_toolBox(){
+  return Container(
+    color: Colors.white,
+    margin: EdgeInsets.only(top: 10),
+    padding: EdgeInsets.all(8),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: _imgBtn(
+              "收货地址",
+              Icon(
+                MdiIcons.homeAccount,
+              ),
+              "order"),
+        ),
+        Expanded(
+          child: _imgBtn(
+              "商品收藏",
+              Icon(
+                Icons.collections,
+              ),
+              "order"),
+        ),
+        Expanded(
+          child: _imgBtn(
+              "最近浏览",
+              Icon(
+                MdiIcons.history,
+              ),
+              "order"),
+        ),
+        Expanded(
+          child: _imgBtn(
+              "退货售后",
+              Icon(
+                MdiIcons.keyboardReturn,
+              ),
+              "order"),
+        ),
+      ],
+    ),
+  );
+}
 _order() {
   return Container(
+    margin: EdgeInsets.only(top: 10),
+    padding: EdgeInsets.all(8),
+    color: Colors.white,
     child: Column(
-      children: <Widget>[Text("我的订单")],
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Text(
+                "我的订单",
+                style: h2,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                "查看全部 >",
+                style: baseFont,
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: _imgBtn(
+                    "待付款",
+                    Icon(
+                      MdiIcons.paypal,
+                    ),
+                    "order"),
+              ),
+              Expanded(
+                child: _imgBtn("待发货", Icon(Icons.send), "order"),
+              ),
+              Expanded(
+                child: _imgBtn("待收货", Icon(Icons.input), "order"),
+              ),
+              Expanded(
+                child: _imgBtn("待评价", Icon(MdiIcons.flower), "order"),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+_imgBtn(String text, Icon icon, String routeName) {
+  return Container(
+    child: Column(
+      children: <Widget>[icon, Text(text)],
     ),
   );
 }
 
 _my() {
   return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: <Widget>[
-            CircleAvatar(
-                radius: 40,
-                backgroundImage:
-                    new AssetImage("images/head.jpeg") //可以在图片上添加文字等等
-                ),
-            Expanded(
-              flex: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "zzlhr",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Text(
-                      "18888888888",
-                      style: TextStyle(color: Colors.black45),
-                    )
-                  ],
-                ),
-              ),
+    padding: EdgeInsets.all(8),
+    color: Colors.white,
+    child: Row(
+      children: <Widget>[
+        CircleAvatar(
+            radius: 40,
+            backgroundImage: new AssetImage("images/head.jpeg") //可以在图片上添加文字等等
             ),
-            Expanded(
-              child: Text("我的资料 >"),
-            )
-          ],
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "zzlhr",
+                  style: h1,
+                ),
+                Text(
+                  "18888888888",
+                  style: TextStyle(color: Colors.black45),
+                )
+              ],
+            ),
+          ),
         ),
-      ));
+        Expanded(
+          child: Text("我的资料 >"),
+        )
+      ],
+    ),
+  );
 }
