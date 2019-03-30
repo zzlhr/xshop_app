5d9a080855247c237130528167ffb4a6583f237e
zzlhr <2388399752@qq.com>
2019-03-31 01:55:54 +0800

    封装Cell组件

---
+++ lib/component/Cell.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:xshop_app/conf/theme.dart';
+
+class Cell extends StatelessWidget {
+  final title;
+
+  final content;
+
+  final isJump;
+
+  final jumpPageName;
+
+  final type;
+
+  const Cell(
+    this.title, {
+    Key key,
+    this.content,
+    this.isJump,
+    this.jumpPageName,
+    this.type,
+  })  : assert(title != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.fromLTRB(8, 12, 8, 12),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Text(
+                  this.title,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Expanded(
+                flex: 20,
+                child: _getContent(),
+              ),
+              _jump()
+            ],
+          ),
+        ),
+        Divider(
+          height: 1,
+        )
+      ],
+    );
+  }
+
+  _getContent() {
+    var _type = type ?? "text";
+    if (_type == "text") {
+      var _content = content ?? "";
+      return Text(_content, textDirection: TextDirection.rtl);
+    }
+    return Container(width: 0, height: 0);
+  }
+
+  _jump() {
+    if (this.isJump) {
+      return Expanded(
+        flex: 1,
+        child: Icon(
+          Icons.chevron_left,
+          textDirection: TextDirection.rtl,
+        ),
+      );
+    }
+    return Container(width: 0, height: 0);
+  }
+}
