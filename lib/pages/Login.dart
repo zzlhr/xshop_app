978b6ac59fe7d5a53ee1edf8d947d3519ed02430
zzlhr <2388399752@qq.com>
2019-04-19 20:59:40 +0800

    登录注册

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:xshop_app/conf/theme.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key, this.title}) : super(key: key);
@@ -19,6 +20,52 @@ class LoginPageState extends State<LoginPage> {
       ),
       body: Column(
         children: <Widget>[
+          // TODO: Logo后续加入
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.transparent)),
+              //输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
+              contentPadding: EdgeInsets.all(10.0),
+              fillColor: Colors.white,
+              filled: true,
+              labelText: '手机号',
+              // 以下属性可用来去除TextField的边框
+              disabledBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.transparent)),
+              //输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
+              contentPadding: EdgeInsets.all(10.0),
+              fillColor: Colors.white,
+              filled: true,
+              labelText: '密码',
+              // 以下属性可用来去除TextField的边框
+              disabledBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+            ),
+            obscureText: true,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(8),
+            child: RaisedButton(
+              color: themeColor,
+              onPressed: () {},
+              child: Text(
+                "登录",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
         ],
       ),
     );
--- lib/pages/Register.dart
+++ lib/pages/Register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:xshop_app/conf/theme.dart';
 
 class RegisterPage extends StatefulWidget {
   RegisterPage({Key key, this.title}) : super(key: key);
@@ -19,6 +20,51 @@ class RegisterPageState extends State<RegisterPage> {
       ),
       body: Column(
         children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.transparent)),
+              //输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
+              contentPadding: EdgeInsets.all(10.0),
+              fillColor: Colors.white,
+              filled: true,
+              labelText: '手机号',
+              // 以下属性可用来去除TextField的边框
+              disabledBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.transparent)),
+              //输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
+              contentPadding: EdgeInsets.all(10.0),
+              fillColor: Colors.white,
+              filled: true,
+              labelText: '密码',
+              // 以下属性可用来去除TextField的边框
+              disabledBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+            ),
+            obscureText: true,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(8),
+            child: RaisedButton(
+              color: themeColor,
+              onPressed: () {},
+              child: Text(
+                "注册",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
         ],
       ),
     );
